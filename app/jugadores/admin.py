7af672651dcb1a8355e9ebb0a1d110d5e90f018c
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-10-01 20:49:44 -0500

    Implementando modelo jugador parcial

--- app/jugadores/admin.py
+++ app/jugadores/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Jugador, JugadorHabilidades
 
 # Register your models here.
+admin.site.register(Jugador)
+admin.site.register(JugadorHabilidades)
--- app/jugadores/models.py
+++ app/jugadores/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Jugador(models.Model):
+    name = models.CharField("Nombre", max_length=40)
+    surname = models.CharField("Apellidos", max_length=40)
+    age = models.IntegerField("Edad", blank=False, null=False)
+
+    def __str__(self):
+        return self.name + " " + self.surname
+
+
+class JugadorHabilidades(models.Model):
+    class Habilidades(models.IntegerChoices):
+        MALO = 1
+        PESIMO = 2
+        REGULAR = 3
+        SOBRESALIENTE = 4
+        EXCELENTE = 5
+
+    jugador = models.OneToOneField(Jugador, on_delete=models.CASCADE)
+    form = models.IntegerChoices("Forma", max_length=20)
+    experience = models.CharField("Experiencia", max_length=20)
+    discipline = models.CharField("Disciplina", max_length=20)
+    stamina = models.CharField("Condición", max_length=20)
+    pace = models.CharField("Rápidez", max_length=20)
+    technique = models.CharField("Técnica", max_length=20)
+    keeper = models.CharField("Guardameta", max_length=20)
+    defending = models.CharField("Defensa", max_length=20)
+    passing = models.CharField("Pases", max_length=20)
+    playmaking = models.CharField("Creación", max_length=20)
+    scoring = models.CharField("Anotación", max_length=20)
+
+    def __str__(self) -> str:
+        return self.id
